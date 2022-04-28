2f72bd255d56a73fed635cdf2f6bddd34d0b0673
Howard <howard_bb@hotmail.com>
2022-04-28 15:56:04 +0800

    update: add composer.json

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '2caf4a2d8c5988731c3def62611f9c1e501af862',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '2caf4a2d8c5988731c3def62611f9c1e501af862',
+            'dev_requirement' => false,
+        ),
+    ),
+);
