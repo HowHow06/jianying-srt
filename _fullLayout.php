ae9d83626ddb273bd4809b9702dc769b71bd7850
Howard <howard_bb@hotmail.com>
2022-06-19 17:38:10 +0800

    upgrade to bootstrap 4

---
+++ _fullLayout.php
@@ -0,0 +1,250 @@
+<!DOCTYPE html>
+<html class="js flexbox canvas canvastext webgl no-touch geolocation postmessage websqldatabase indexeddb hashchange history draganddrop websockets rgba hsla multiplebgs backgroundsize borderimage borderradius boxshadow textshadow opacity cssanimations csscolumns cssgradients cssreflections csstransforms csstransforms3d csstransitions fontface generatedcontent video audio localstorage sessionstorage webworkers no-applicationcache svg inlinesvg smil svgclippaths">
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <style class="vjs-styles-defaults">
+    .video-js {
+      width: 300px;
+      height: 150px;
+    }
+
+    .vjs-fluid {
+      padding-top: 56.25%;
+    }
+  </style>
+  <title><?php echo ($title . " - Ho2" ?? "Ho2"); ?></title>
+
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="robots" content="index,follow" />
+  <meta name="viewport" content="width=device-width, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
+  <meta name="format-detection" content="telphone=no, email=no" />
+  <meta name="description" content="" />
+  <meta name="keywords" content="Jian Ying Convert to SRT" />
+  <meta name="description" itemprop="description" content="" />
+  <meta name="robots" content="max-image-preview:large" />
+  <link rel="stylesheet" id="global-styles-css" href="./bundle/global.css" type="text/css" media="all" />
+  <link rel="stylesheet" id="ce_responsive-css" href="./bundle/video-container.min.css" type="text/css" media="all" />
+  <link rel="stylesheet" id="animate-css" href="./bundle/animate.min.css" type="text/css" media="all" />
+  <link rel="stylesheet" id="awesome-css" href="./assets/fontawesome/css/all.min.css" type="text/css" media="all" />
+  <link rel="stylesheet" id="awesome-solid-css" href="./assets/fontawesome/css/solid.min.css" type="text/css" media="all" />
+  <link rel="stylesheet" id="bootstrap-css" href="./bundle/vendors/bootstrap4/css/bootstrap.min.css" type="text/css" media="all" />
+  <link rel="stylesheet" id="superfish-css" href="./bundle/vendors/superfish/superfish.css" type="text/css" media="all" />
+  <link rel="stylesheet" id="kratos-css" href="./bundle/style.css" type="text/css" media="all" />
+  <link rel="stylesheet" id="fancybox-css" href="./bundle/jquery.fancybox.min.css" type="text/css" media="all" />
+  <link rel="stylesheet" id="style-videojs-css" href="./bundle/video-js.css" type="text/css" media="all" />
+  <link rel="icon" href="assets\logo\cropped-ho2-logos_white.png" sizes="32x32" />
+  <link rel="icon" href="assets\logo\cropped-ho2-logos_white.png" sizes="192x192" />
+  <link rel="apple-touch-icon" href="assets\logo\cropped-ho2-logos_white.png" />
+  <meta name="msapplication-TileImage" content="assets\logo\cropped-ho2-logos_white.png" />
+  <script type="text/javascript" src="./bundle/jquery.min.js" id="jquery-js"></script>
+</head>
+
+<body data-spy="scroll" data-target=".scrollspy" data-new-gr-c-s-check-loaded="14.1058.0" data-gr-ext-installed="" data-new-gr-c-s-loaded="14.1058.0">
+  <div id="kratos-wrapper">
+    <div id="kratos-page">
+      <nav id="offcanvas-menu" class="menu-%e9%a1%b6%e9%83%a8%e8%8f%9c%e5%8d%95-container" style="height: 703px">
+        <!-- <ul id="" class="" style="touch-action: pan-y">
+            <li>
+              <a class="sf-with-ul"
+                ><i class="fa fa-cog" aria-hidden="true"></i> 项目</a
+              >
+              <ul class="sub-menu" style="display: none">
+                <li>
+                  <a href="#"
+                    >黑鸟播放器</a
+                  >
+                </li>
+                <li>
+                  <a href="#"
+                    >TeleList工具</a
+                  >
+                </li>
+                <li>
+                  <a href="#"
+                    >在线格式转换</a
+                  >
+                </li>
+                <li>
+                  <a href="#">剪映工具</a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a><i class="fa fa-code" aria-hidden="true"></i>工具</a>
+            </li>
+            <li>
+              <a class="sf-with-ul"
+                ><i class="fa fa-gitlab" aria-hidden="true"></i> 关于</a
+              >
+              <ul class="sub-menu" style="display: none">
+                <li><a href="#">关于本站</a></li>
+                <li>
+                  <a
+                    target="_blank"
+                    rel="noopener"
+                    href="#"
+                    >文章归档</a
+                  >
+                </li>
+                <li>
+                  <a href="#">留言板</a>
+                </li>
+                <li>
+                  <a class="sf-with-ul">网站管理</a>
+                  <ul class="sub-menu" style="display: none">
+                    <li><a href="#">登录</a></li>
+                    <li>
+                      <a href="#?action=register"
+                        >注册</a
+                      >
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a href="#"
+                ><i class="fa fa-gg" aria-hidden="true"></i> 链接</a
+              >
+            </li>
+          </ul> -->
+      </nav>
+      <div id="kratos-header">
+        <header id="kratos-header-section">
+          <div class="container">
+            <div class="nav-header">
+              <h1 id="kratos-logo">
+                <a href="./">
+                  <img src="assets\logo\trimmed-Ho2-logos_white.png" alt="logo" height="35px" />
+                </a>
+              </h1>
+              <nav id="kratos-menu-wrap" class="menu-%e9%a1%b6%e9%83%a8%e8%8f%9c%e5%8d%95-container">
+                <ul id="kratos-primary-menu" class="sf-menu sf-js-enabled sf-arrows" style="touch-action: pan-y">
+                  <li>
+                    <a class="sf-with-ul"><i class="fa fa-code" aria-hidden="true"></i> 工具 </a>
+                    <ul class="sub-menu" style="display: none">
+                      <li>
+                        <a href="./">剪映字幕导出工具</a>
+                      </li>
+                      <li>
+                        <a href="./text-convertor">中文转换工具</a>
+                      </li>
+                    </ul>
+                  </li>
+                  <li>
+                    <a class="sf-with-ul"><i class="fa-brands fa-github" aria-hidden="true"></i> Github 项目 </a>
+                    <ul class="sub-menu" style="display: none">
+                      <li>
+                        <a href="https://github.com/HowHow06/pptGenerator" target="_blank">Excel-PPT生成</a>
+                      </li>
+                    </ul>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+        </header>
+      </div>
+      <div class="kratos-start kratos-hero-2 kratos-post-header">
+        <div class="kratos-overlay kratos-post" style="background: #230237"></div>
+      </div>
+      <style type="text/css" rel="stylesheet">
+        label {
+          user-select: none;
+        }
+
+        .texttosrt {
+          width: 100%;
+          height: 320px;
+        }
+
+        .form-group {
+          float: right;
+          margin-top: 5px;
+          margin-left: 21px;
+          margin-bottom: 10px;
+        }
+
+        .jytosrt-szhhf {
+          float: right;
+          margin-top: 5px;
+          margin-left: 26px;
+        }
+
+        .fileinput-button {
+          float: left;
+          position: relative;
+          display: inline-block;
+          overflow: hidden;
+          margin-top: 0px;
+          margin-bottom: 10px;
+        }
+
+        .fileinput-button input {
+          position: absolute;
+          right: 0;
+          top: 0;
+          opacity: 0;
+          -ms-filter: "alpha(opacity=0)";
+          font-size: 20px;
+        }
+
+        .kratos-post-inner {
+          padding-top: 2px;
+        }
+
+        @media screen and (max-width: 768px) {
+          .kratos-post-inner {
+            padding-top: 2px;
+          }
+        }
+
+        .kratos-entry-title {
+          margin: 18px 0px 18px 0px;
+          font-size: 18px;
+          font-weight: bold;
+        }
+
+        .mysidebartool li a {
+          color: #3f3f3f;
+        }
+      </style>
+      <div id="kratos-blog-post" style="background: #ffffff">
+        <!-- content here -->
+        <?php if (isset($childView)) {
+          include($childView);
+        }  ?>
+      </div>
+      <footer>
+        <div id="footer">
+          <div class="cd-tool visible text-center">
+            <a class="cd-top cd-is-visible cd-fade-out"><span class="fa fa-chevron-up"></span></a>
+          </div>
+          <div class="container">
+            <div class="row justify-content-md-center">
+              <div class="col-md-6 col-md-offset-3 footer-list text-center">
+                <p class="kratos-social-icons"></p>
+                <p>HO2 Web App</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </div>
+  </div>
+  <script type="text/javascript" src="./bundle/jquery.easing.min.js" id="easing-js"></script>
+  <script type="text/javascript" src="./bundle/jquery.qrcode.min.js" id="qrcode-js"></script>
+  <script type="text/javascript" src="./bundle/modernizr.min.js" id="modernizr-js"></script>
+  <!-- <script type="text/javascript" src="./bundle/bootstrap.min.js" id="bootstrap-js"></script> -->
+  <script type="text/javascript" src="./bundle/vendors/bootstrap4/js/bootstrap.bundle.js" id="bootstrap-js"></script>
+  <script type="text/javascript" src="./bundle/jquery.waypoints.min.js" id="waypoints-js"></script>
+  <script type="text/javascript" src="./bundle/jquery.stellar.min.js" id="stellar-js"></script>
+  <script type="text/javascript" src="./bundle/vendors/superfish/hoverIntent.js" id="hoverIntents-js"></script>
+  <script type="text/javascript" src="./bundle/vendors/superfish/superfish.min.js" id="superfish-js"></script>
+  <script type="text/javascript" src="./bundle/kratos.js" id="kratos-js"></script>
+  <script type="text/javascript" src="./bundle/jquery.fancybox.min.js" id="fancybox-js"></script>
+  <script type="text/javascript" src="./bundle/video-js.min.js" id="script-videojs-js"></script>
+</body>
+
+</html>
